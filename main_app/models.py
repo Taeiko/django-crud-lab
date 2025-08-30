b3e1fe735131f6fc1f739ef26e198b1281d096ea
Is an English Major <Najeebihesa@gmail.com>
2025-08-30 21:23:37 +0300

    connected database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Console(models.Model):
+    name = models.CharField(max_length=80, unique=False)
+    release_year = models.IntegerField()
+    is_worth_playing= models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    class Meta:
+        db_table= 'game-consoles'
+        
+    def __str__(self):
+            return f'name:{self.name}, is it worth playing? : {self.is_worth_playing}'
